a947f38ea147ed9827625ea67df0c19d3efb0d5d
jacktherock <sonawaneabhijeet273@gmail.com>
2023-02-21 18:30:42 +0530

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid justify-content-center">
-          <a className="navbar-brand" href="#/">
+          <a className="navbar-brand text-uppercase" href="#/">
             <img
               src="https://cdn-icons-png.flaticon.com/512/7014/7014378.png"
               width={50}
